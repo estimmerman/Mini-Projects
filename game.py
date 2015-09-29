d56ab6e659a458449482e8a4c1c85ad0b9a51308
Eric Timmerman <estimmerman@gmail.com>
2015-09-29 00:21:40 -0400

    delay for url requests, successfully find key

--- game.py
+++ game.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 from random import randint
 import urllib2
 import urllib
+import time
+import re
 
 def open_url(url):
         return BeautifulSoup(urllib2.urlopen(url).read(), "html.parser")
@@ -11,10 +13,9 @@ def run_game(start_topic, key):
 	wiki_param = "/wiki/"
 	
 	page_url = wiki_url + wiki_param + urllib.quote(start_topic)
-	print "Searching Article on: " + urllib.unquote(page_url)
 	
-	notFound = True
-	while notFound:
+	found = False
+	while found == False:
 		page = open_url(page_url)
 		article_title = page.find_all('h1', id="firstHeading")[0].string
                 print "Searching Article on: " + article_title
@@ -24,16 +25,17 @@ def run_game(start_topic, key):
 			if is_good_wiki_link(a['href']):
 				wiki_links.append(a['href'])
 	
-		if len(page.body.find_all(text=key)) > 0:
-			print "Success! Found word in article: " + urllib.unquote(page_url)
-			notFound = False		
-		else
+		print page.body.find_all(text="the")
+		if len(page.body.find_all(text=re.compile(key))) > 0:
+			print "Success! Found word in article: " + article_title
+			found = True		
+		else:
 			page_url = wiki_url + wiki_links[randint(0,len(wiki_links) - 1)]
-
+			time.sleep(3)
 
 def is_good_wiki_link(link):
 	return ":" not in link and link.startswith("/wiki/")
 
 
-run_game("World War II", "bee")
+run_game("World War II", "global")
 
