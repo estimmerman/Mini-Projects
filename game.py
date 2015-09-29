f0b0437aef957099d5612f7695f3158443683828
Eric Timmerman <estimmerman@gmail.com>
2015-09-28 23:08:22 -0400

    better filtering of bad links

--- game.py
+++ game.py
@@ -7,18 +7,23 @@ def open_url(url):
         return BeautifulSoup(urllib2.urlopen(url).read(), "html.parser")
 
 def run_game(start_topic, key):
-	wiki_url = "http://en.wikipedia.org/wiki/"
-	page_url = wiki_url + urllib.quote(start_topic)
-	found = False
-	while(found == False):
+	wiki_url = "http://en.wikipedia.org"
+	wiki_param = "/wiki/"
+	page_url = wiki_url + wiki_param + urllib.quote(start_topic)
+	notFound = True
+	while notFound:
 		page = open_url(page_url)
 		wiki_links = []
 		for a in page.find_all('a', href=True):
-			if "/wiki/" in a['href']:
+			if is_good_wiki_link(a['href']):
 				wiki_links.append(a['href'])
-		page_url = wiki_url + wiki_links[randint(0,len(wiki_links) - 1)]
+		print wiki_links
+		page_url = wiki_url + urllib.unquote(wiki_links[randint(0,len(wiki_links) - 1)])
 		print page_url
-		found == True
+		notFound = False
+
+def is_good_wiki_link(link):
+	return ":" not in link and link.startswith("/wiki/")
 
 
 run_game("World War II", "bee")
